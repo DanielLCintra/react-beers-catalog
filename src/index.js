fbf3174fe110a66455d60ffe699f914f6cbf4833
Daniel Cintra <daniel.cintra@jn2.com.br>
2018-02-05 10:21:52 -0200

    switch version of bootstrap

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ import Dashboard from './features/dashboard/index.jsx'
 
 ReactDOM.render(
 	(
-	    <Router history={Router.hashHistory}>
-      		<App>
-        		<Switch>
-	          		<Route exact path="/" component={Dashboard} />
-	          		<Route path="/beers" component={Beers} />
-	         	 	<Route path="/styles" component={Styles} />
-        		</Switch>
-      		</App>
-    	</Router>
+    <Router history={Router.hashHistory}>
+    		<App>
+      		<Switch>
+        		<Route exact path="/" component={Dashboard} />
+        		<Route path="/beers" component={Beers} />
+       	 	  <Route path="/styles" component={Styles} />
+      		</Switch>
+    		</App>
+  	</Router>
 	),
 	document.getElementById('root')
 );
